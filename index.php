d017244f71700cd6b1a421e8baee324db87354e7
alokkumar-dev <alokshakya2601@gmail.com>
2021-01-12 00:38:32 +0530

    assa

--- index.php
+++ index.php
@@ -4,15 +4,47 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
+    <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- Font awesome CSS -->
+    <link rel="stylesheet" href="css/all.min.css">
 
-    <title>Document</title>
+    <!-- Google Font -->
+<link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@700&display=swap" rel="stylesheet">
+     <!-- Custom CSS -->
+     <link rel="stylesheet" href="css/style.css">
+    <title>iSchool</title>
 </head>
 <body>
-    
-<script src="js/popper.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
+     <!-- Start Navigation -->
+ <nav class="navbar navbar-expand-sm navbar-dark bg-dark  ">
+   <div class="container-fluid">
+    <a class="navbar-brand" href="index.php">iSchool</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav custom-nav">
+        <li class="nav-item custom-nav-item"><a href="index.php" class="nav-link">Home</a></li>
+        <li class="nav-item custom-nav-item"><a href="#" class="nav-link">Courses</a></li>
+        <li class="nav-item custom-nav-item"><a href="#" class="nav-link">My Profile</a></li>
+        <li class="nav-item custom-nav-item"><a href="#" class="nav-link">Logout</a></li>
+        <li class="nav-item custom-nav-item"><a href="#" class="nav-link">Login</a></li>
+        <li class="nav-item custom-nav-item"><a href="#" class="nav-link">Signup</a></li>
+        <li class="nav-item custom-nav-item"><a href="#" class="nav-link">Feedback</a></li>
+        <li class="nav-item custom-nav-item"><a href="#" class="nav-link">Contact-Us</a></li>
 
+      </ul>
+    </div>
+  </div>
+</nav>
+     <!-- End Navigation -->
+
+     <!-- Bootstrap JavaScipt -->
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <!-- Font awesome JS -->
+    <script src="js/all.min.js"></script>
 
 </body>
 </html>
